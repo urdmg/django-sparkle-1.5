5af4a6ad5ceaa4f25518068a33bc8aff3e08df33
Johannes Spielmann <jps@shezi.de>
2013-08-13 14:35:55 +0200

    fixing old import (pre-1.4)

--- sparkle/urls.py
+++ sparkle/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 urlpatterns = patterns('sparkle.views',
     url(r'^(?P<application_slug>[\w-]+)/appcast.xml$', 'appcast', name='sparkle_application_appcast'),
