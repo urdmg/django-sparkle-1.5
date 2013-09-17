341cca7603c6c637cc9f211286cf8467e75898b4
Johannes Spielmann <jps@shezi.de>
2013-09-17 11:48:02 +0200

    schema migrations for `Application.filename_slug`

---
+++ sparkle/migrations/0009_set_filename_slug.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        "Write your forwards methods here."
+        for application in orm['sparkle.Application'].objects.all():
+            application.filename_slug = application.slug
+            application.save()
+        
+    def backwards(self, orm):
+        "Write your backwards methods here."
+        pass
+
+    models = {
+        u'sparkle.application': {
+            'Meta': {'object_name': 'Application'},
+            'filename_slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50'})
+        },
+        u'sparkle.systemprofilereport': {
+            'Meta': {'object_name': 'SystemProfileReport'},
+            'added': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'ip_address': ('django.db.models.fields.IPAddressField', [], {'max_length': '15'})
+        },
+        u'sparkle.systemprofilereportrecord': {
+            'Meta': {'object_name': 'SystemProfileReportRecord'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'report': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sparkle.SystemProfileReport']"}),
+            'value': ('django.db.models.fields.CharField', [], {'max_length': '80'})
+        },
+        u'sparkle.version': {
+            'Meta': {'object_name': 'Version'},
+            'application': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['sparkle.Application']"}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'dsa_signature': ('django.db.models.fields.CharField', [], {'max_length': '80', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'length': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'minimum_system_version': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'publish_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'release_notes': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'short_version': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'update': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'version': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['sparkle']
+    symmetrical = True
